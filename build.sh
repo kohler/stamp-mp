0b8808346189493fead63e2f821787a740ba9d75
Dave Boutcher <daveboutcher@gmail.com>
2012-01-16 12:00:10 -0600

    Add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROGS="bayes genome intruder kmeans labyrinth ssca2 vacation yada"
+TARGETS="seq stm"
+
+for t in ${TARGETS}
+do
+    for p in ${PROGS}
+    do
+    	make -C ${p} -f  Makefile TARGET=${t} $*
+	if [ $? -ne 0 ]; then
+	    exit -1
+	fi
+    done     	
+done
