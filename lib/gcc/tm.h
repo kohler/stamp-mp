3877c8c77fe6bb21bb9e27e26faa99d5cdb52550
Dave Boutcher <daveboutcher@gmail.com>
2012-04-18 13:42:31 -0500

    gcc stm support: build files

---
+++ lib/gcc/tm.h
@@ -0,0 +1,181 @@
+/* =============================================================================
+ *
+ * tinystm/tm.h
+ *
+ * Utility defines for transactional memory
+ *
+ * =============================================================================
+ *
+ * Copyright (C) Dave Boutcher, 2012.  All Rights Reserved.
+ *
+ * =============================================================================
+ *
+ * =============================================================================
+ */
+
+
+#ifndef TM_H
+#define TM_H 1
+
+#include "../tm_common.h"
+
+/* =============================================================================
+ * Transactional Memory System Interface
+ *
+ * TM_ARG
+ * TM_ARG_ALONE
+ * TM_ARGDECL
+ * TM_ARGDECL_ALONE
+ *     Used to pass TM thread meta data to functions (see Examples below)
+ *
+ * TM_STARTUP(numThread)
+ *     Startup the TM system (call before any other TM calls)
+ *
+ * TM_SHUTDOWN()
+ *     Shutdown the TM system
+ *
+ * TM_THREAD_ENTER()
+ *     Call when thread first enters parallel region
+ *
+ * TM_THREAD_EXIT()
+ *     Call when thread exits last parallel region
+ *
+ * P_MALLOC(size)
+ *     Allocate memory inside parallel region
+ *
+ * P_FREE(ptr)
+ *     Deallocate memory inside parallel region
+ *
+ * TM_MALLOC(size)
+ *     Allocate memory inside atomic block / transaction
+ *
+ * TM_FREE(ptr)
+ *     Deallocate memory inside atomic block / transaction
+ *
+ * TM_BEGIN()
+ *     Begin atomic block / transaction
+ *
+ * TM_BEGIN_RO()
+ *     Begin atomic block / transaction that only reads shared data
+ *
+ * TM_END()
+ *     End atomic block / transaction
+ *
+ * TM_RESTART()
+ *     Restart atomic block / transaction
+ *
+ * TM_EARLY_RELEASE()
+ *     Remove speculatively read line from the read set
+ *
+ * =============================================================================
+ *
+ * Example Usage:
+ *
+ *     MAIN(argc,argv)
+ *     {
+ *         TM_STARTUP(8);
+ *         // create 8 threads and go parallel
+ *         TM_SHUTDOWN();
+ *     }
+ *
+ *     void parallel_region ()
+ *     {
+ *         TM_THREAD_ENTER();
+ *         subfunction1(TM_ARG_ALONE);
+ *         subfunction2(TM_ARG  1, 2, 3);
+ *         TM_THREAD_EXIT();
+ *     }
+ *
+ *     void subfunction1 (TM_ARGDECL_ALONE)
+ *     {
+ *         TM_BEGIN_RO()
+ *         // ... do work that only reads shared data ...
+ *         TM_END()
+ *
+ *         long* array = (long*)P_MALLOC(10 * sizeof(long));
+ *         // ... do work ...
+ *         P_FREE(array);
+ *     }
+ *
+ *     void subfunction2 (TM_ARGDECL  long a, long b, long c)
+ *     {
+ *         TM_BEGIN();
+ *         long* array = (long*)TM_MALLOC(a * b * c * sizeof(long));
+ *         // ... do work that may read or write shared data ...
+ *         TM_FREE(array);
+ *         TM_END();
+ *     }
+ *
+ * =============================================================================
+ */
+
+
+/* =============================================================================
+ * STM - Software Transactional Memory
+ * =============================================================================
+ */
+
+#include <string.h>
+#include "thread.h"
+
+#define TM_ARG                        /* nothing */
+#define TM_ARG_ALONE                  /* nothing */
+#define TM_ARGDECL                    /* nothing */
+#define TM_ARGDECL_ALONE              /* nothing */
+#define TM_CALLABLE                   __attribute__((transaction_safe))
+
+#define TM_STARTUP(numThread)         /* nothing */
+#define TM_SHUTDOWN()                 /* nothing */
+
+#define TM_THREAD_ENTER()             /* nothing */
+#define TM_THREAD_EXIT()              /* nothing */
+
+#define P_MALLOC(size)            malloc(size)
+#define P_FREE(ptr)               free(ptr)
+#define TM_MALLOC(size)           malloc(size)
+#define TM_FREE(ptr)              free(ptr)
+
+#define TM_START(ro)                 /* nothing */
+#define TM_BEGIN()                __transaction_atomic {
+#define TM_BEGIN_RO()             __transaction_atomic {k  TM_START(1)
+#define TM_END()                  }
+#define TM_RESTART()              transaction_cancel
+
+#define TM_EARLY_RELEASE(var)       /* nothing */
+
+/* =============================================================================
+ * Transactional Memory System interface for shared memory accesses
+ *
+ * There are 3 flavors of each function:
+ *
+ * 1) no suffix: for accessing variables of size "long"
+ * 2) _P suffix: for accessing variables of type "pointer"
+ * 3) _F suffix: for accessing variables of type "float"
+ * =============================================================================
+ */
+#define TM_SHARED_READ(var)           (var)
+#define TM_SHARED_READ_P(var)         (var)
+#define TM_SHARED_READ_F(var)         (var)
+
+#define TM_SHARED_WRITE(var, val)     ({var = val; var;})
+#define TM_SHARED_WRITE_P(var, val)   ({var = val; var;})
+#define TM_SHARED_WRITE_F(var, val)   ({var = val; var;})
+
+#define TM_LOCAL_WRITE(var, val)      ({var = val; var;})
+#define TM_LOCAL_WRITE_P(var, val)    ({var = val; var;})
+#define TM_LOCAL_WRITE_F(var, val)    ({var = val; var;})
+
+/* This is a hack */
+TM_CALLABLE double log(double x);
+TM_CALLABLE void __assert_fail (__const char *__assertion, __const char *__file,
+			   unsigned int __line, __const char *__function)
+    __THROW __attribute__ ((__noreturn__, transaction_safe));
+#endif /* TM_H */
+
+
+/* =============================================================================
+ *
+ * End of tinystm/tm.h
+ *
+ * =============================================================================
+ */
---
+++ lib/gcchack.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include <math.h>
+
+double _ZGTt3log(double x) { return log(x); }
+
+void _ZGTt13__assert_fail (__const char *__assertion, __const char *__file,
+			   unsigned int __line, __const char *__function)
+{
+    __assert_fail(__assertion, __file, __line, __function);
+}
+
